35b317711a54d1ecaa7f3f07c0127c1790bb8806
mschaffrath <mscha008@ucr.edu>
2017-10-22 04:17:46 -0700

    added formatting, finalized uniform cost

--- HW1_8Puzzle.py
+++ HW1_8Puzzle.py
@@ -9,15 +9,19 @@ class Node(object):
   def __init__(self,state):
     self.state = state
 
-initState = [[1,  2,  3 ],
-             [4,  5,  6 ],
-             [7, "b", 8 ]]
-goalState = [[1,  2,  3 ],
-             [4,  5,  6 ],
-             [7,  8, "b"]]
+goalState = [['1','2','3'],
+             ['4','5','6'],
+             ['7','8','b']]
+
+expandCount = 0
+queueSize = 0
+goalDepth = 0
+
+def PrintFormattedState(state):
+  for i in range(len(state)):
+      print(state[i])
 
 def MakeNode(initState):
-  print("Make Node called")
   return Node(initState) 
 
 def RemoveFront(nodes):
@@ -30,38 +34,30 @@ def GoalTest(node):
 
 class Operators():
   def MoveBlankLeft(self, node, coord):
-    print("move left called")
     x = coord[0]
     y = coord[1]
     tempVal = node.state[x][y-1]
-    print("tempVal: ", tempVal)
     node.state[x][y] = tempVal
     node.state[x][y-1] = "b" 
     return node
   def MoveBlankRight(self, node, coord):
-    print("move right called on: ", node.state)
     x = coord[0]
     y = coord[1]
     tempVal = node.state[x][y+1]
-    print("tempVal: ", tempVal)
     node.state[x][y] = tempVal
     node.state[x][y+1] = "b" 
     return node
   def MoveBlankUp(self, node, coord):
-    print("move up called on: ", node.state)
     x = coord[0]
     y = coord[1]
     tempVal = node.state[x-1][y]
-    print("tempVal: ", tempVal)
     node.state[x][y] = tempVal
     node.state[x-1][y] = "b" 
     return node
   def MoveBlankDown(self, node, coord):
-    print("move down called")
     x = coord[0]
     y = coord[1]
     tempVal = node.state[x+1][y]
-    print("tempVal: ", tempVal)
     node.state[x][y] = tempVal
     node.state[x+1][y] = "b" 
     return node
@@ -80,7 +76,6 @@ def Expand(node, ops):
       continue
     x = i
   coord = (x,y)
-  print("coord: ", x , " " , y) 
   
   if (x == 1 or x == 0):
     newNodes.append(ops.MoveBlankDown(downNode, coord))
@@ -91,59 +86,87 @@ def Expand(node, ops):
   if (y == 1 or y == 0):
     newNodes.append(ops.MoveBlankLeft(leftNode, coord))
 
-  print ("new nodes: ")
-  print (newNodes[0].state[0])
-  print (newNodes[0].state[1])
-  print (newNodes[0].state[2])
-  print (newNodes[1].state[0])
-  print (newNodes[1].state[1])
-  print (newNodes[1].state[2])
-   
   return newNodes
 
-# def QueueingFunction(nodes, listOfNewNodes):
 
+def QueueingFunction(nodes, listOfNewNodes):
+  for i in range(len(listOfNewNodes)):
+    nodes.append(listOfNewNodes[i])
+  return nodes
 
-# TESTING *************************
-MakeNode(initState)
-
-def GeneralSearch(initState):
+prevQMax = 0
+def GeneralSearch(initState, QueueingFunction):
+  global expandCount
+  global queueSize
+  global goalDepth
+  initState = initState.state
   nodes = []
   nodes.append(MakeNode(initState))
+  prevQmax = len(nodes)
 
   while(True):
-    
+    global prevQMax
+    queueSize = max(prevQMax, len(nodes))
+    prevQmax = queueSize
+
     if not nodes:
       print ("failure")
       return False
     
     node = RemoveFront(nodes)
-    print("node after remove")
-    print(node.state)
 
     if GoalTest(node):
-      print ("success")
+      print ("Success")
       return True
-    
-    ops = Operators()
-    Expand(node, ops)
 
-    return False
+    expandCount += 1
+    print("Expanding Node: ")
+    PrintFormattedState(node.state)
+    nodes = QueueingFunction(nodes, Expand(node, Operators()))
 
-#    nodes = QueueingFunction(nodes, Expand(node, Operators))
+defaultInitState = [['1','2','3'],
+                    ['4','5','6'],
+                    ['7','b','8']]
+
+print ("PROGRAM START")
+
+print ("Welcome to Mathew Schaffrath 8-puzzle solver.")
+print ("Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle.")
+selectPuzzle = input("")
+
+if selectPuzzle == "2":
+  userInitState = []
+  print ("Enter your puzzle, use a zero to represent the blank")
+  row1 = input("Enter the first row, use space or tabes between numbers   ")
+  userInitState.append(row1.split())
+  row2 = input("Enter the first row, use space or tabes between numbers   ")
+  userInitState.append(row2.split())
+  row3 = input("Enter the first row, use space or tabes between numbers   ")
+  userInitState.append(row3.split())
+  print("\n")
+  problem = MakeNode(userInitState)
+elif selectPuzzle == "1":
+  problem = MakeNode(defaultInitState)
+
+print("Enter your choice of algorithm")
+print("\t", "1.  Uniform Cost Search")
+print("\t", "2.  A* with the Misplaced Tile heuristic")
+print("\t", "3.  A* with the Manhattan distance heuristic")
+selectAlg = input("         ")
+
+print("Initial State: ")
+PrintFormattedState(problem.state)
+if selectAlg == "1":
+  GeneralSearch(problem, QueueingFunction)
+if selectAlg == "2":
+  print("2")
+
+print("Nodes expanded: ", expandCount)
+print("Max # of nodes in queue at any one time: ", queueSize)
+
+print ("PROGRAM END")
 
 
-print ("START")
-
-print("GenSearch:")
-GeneralSearch(initState)
 
 
-print ("END")
-
-
-
-
-#print ("Welcome to Mathew Schaffrath 8-puzzle solver.")
-#print ("Type "1" to use a default puzzle, or "2" to enter your own puzzle.")
 
