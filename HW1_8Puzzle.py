b21ba256b2f1bf350c3d9996ee2b6a2db3f92d2b
mschaffrath <mscha008@ucr.edu>
2017-10-25 10:09:39 -0700

    Added Set to check for repeat states. All test cases work

--- HW1_8Puzzle.py
+++ HW1_8Puzzle.py
@@ -1,21 +1,36 @@
 #
 #  CS170 - Artifical Intelligence
-#
+#  8 - Puzzle Solver
 #  Author: Mathew Schaffrath
 #
 
 import copy
 import operator
 
+# Node Object to store STATE and COSTS
 class Node(object):
   def __init__(self,state):
     self.state = state
+  def SetParent(self, parent):
+    self.parent = parent
+  parent = None
+  depth = 0
   gn = 0
-  hn = 0
+  hn = 0 
+  MDH = 0
+  MTH = 0
+  def GetDepth(self):
+    return self.depth
+  def SetDepth(self, cost):
+    self.depth = cost
   def SetGn(self, cost):
     self.gn = cost
   def SetHn(self, cost):
     self.hn = cost
+  def SetTotalMDH(self):
+    self.MDH = self.depth + self.hn
+  def SetTotalMTH(self):
+    self.MTH = self.depth + self.gn
 
 goalState = [['1','2','3'],
              ['4','5','6'],
@@ -24,10 +39,13 @@ goalState = [['1','2','3'],
 expandCount = 0
 queueSize = 0
 goalDepth = 0
+repeatCheck = set()
 
+# To print state human readable
 def PrintFormattedState(state):
   for i in range(len(state)):
       print(state[i])
+  print("")
 
 def MakeNode(initState):
   return Node(initState) 
@@ -69,8 +87,9 @@ class Operators():
     node.state[x][y] = tempVal
     node.state[x+1][y] = "b" 
     return node
- 
+
 def Expand(node, ops):
+  global expandCount
   newNodes = []
   upNode = copy.deepcopy(node)
   downNode = copy.deepcopy(node)
@@ -86,27 +105,46 @@ def Expand(node, ops):
   coord = (x,y)
 
   if (x == 1 or x == 0):
+    downNode.SetParent(node)
+    downNode.SetDepth(downNode.GetDepth() + 1)
     newNodes.append(ops.MoveBlankDown(downNode, coord))
+#    expandCount += 1
+    t1 = tuple(node.state[0])
+    t1 = t1 + tuple(node.state[1])
+    t1 = t1 + tuple(node.state[2])
+    repeatCheck.add(t1)
+
   if (x == 1 or x == 2):
+    upNode.SetParent(node)
+    upNode.SetDepth(upNode.GetDepth() + 1)
     newNodes.append(ops.MoveBlankUp(upNode, coord))
+#    expandCount += 1
+    t2 = tuple(node.state[0])
+    t2 = t2 + tuple(node.state[1])
+    t2 = t2 + tuple(node.state[2])
+    repeatCheck.add(t2)
+
   if (y == 1 or y == 0):
+    rightNode.SetParent(node)
+    rightNode.SetDepth(rightNode.GetDepth() + 1) 
     newNodes.append(ops.MoveBlankRight(rightNode, coord))
+#    expandCount += 1
+    t3 = tuple(node.state[0])
+    t3 = t3 + tuple(node.state[1])
+    t3 = t3 + tuple(node.state[2])
+    repeatCheck.add(t3)
+
   if (y == 1 or y == 2):
+    leftNode.SetParent(node)
+    leftNode.SetDepth(leftNode.GetDepth() + 1)
     newNodes.append(ops.MoveBlankLeft(leftNode, coord))
+#    expandCount += 1
+    t4 = tuple(node.state[0])
+    t4 = t4 + tuple(node.state[1])
+    t4 = t4 + tuple(node.state[2])
+    repeatCheck.add(t4)
 
-#  newNodes = PruneStates(newNodes)
   return newNodes
-
-def PruneStates(nodes):
-  end = len(nodes)
-  for i in range(end):
-    for j in range(i+1, end):
-      if nodes[i].state == nodes[j].state:
-        print("STATE PRUNED:--------------------------------------")
-        PrintFormattedState(nodes[j].state)
-        del nodes[j]
-        end -= 1
-  return nodes
 
 def CountAndSetMTH(node):
   mthCount = 0
@@ -162,32 +200,46 @@ def UCS(nodes, listOfNewNodes):
     nodes.append(listOfNewNodes[i])
   return nodes
 
+def SetMTHTotal(node):
+  node.SetTotalMTH()
+  return True
+
 def MTH(nodes, listOfNewNodes):
   CalcMTH(listOfNewNodes)
-  #print("UNSORTED:")
-  #for i in range(len(listOfNewNodes)):
-  #  PrintFormattedState(listOfNewNodes[i].state)
-  #print("SORTED:")
-  #for i in range(len(listOfNewNodes)):
-  #  PrintFormattedState(listOfNewNodes[i].state)
-
   for i in range(len(listOfNewNodes)):
     nodes.append(listOfNewNodes[i])
-  nodes = sorted(nodes, key=operator.attrgetter('gn'))
+  for i in range(len(listOfNewNodes)):
+    SetMTHTotal(listOfNewNodes[i])
+  nodes = sorted(nodes, key = operator.attrgetter('MTH'))
   return nodes
+
+def SetMDHTotal(node):
+  node.SetTotalMDH()
+  return True
 
 def MDH(nodes, listOfNewNodes):
   CalcMDH(listOfNewNodes)
   for i in range(len(listOfNewNodes)):
     nodes.append(listOfNewNodes[i])
-  nodes = sorted(nodes, key = operator.attrgetter('hn'))
+  for i in range(len(listOfNewNodes)):
+    SetMDHTotal(listOfNewNodes[i])
+  nodes = sorted(nodes, key = (operator.attrgetter('MDH')))
   return nodes
 
+def GetPath(node):
+  if not node.parent:
+    PrintFormattedState(node.state)
+  else: 
+    GetPath(node.parent)
+    PrintFormattedState(node.state)
+
 prevQMax = 0
+repeat = 0
 def GeneralSearch(initState, QueueingFunction):
   global expandCount
   global queueSize
   global goalDepth
+  global repeat
   initState = initState.state
   nodes = []
   nodes.append(MakeNode(initState))
@@ -201,29 +253,37 @@ def GeneralSearch(initState, QueueingFunction):
     if not nodes:
       print ("failure")
       return False
-    
-    node = RemoveFront(nodes)
+   
+    while nodes:
+      node = RemoveFront(nodes)
+      tg = tuple(node.state[0])
+      tg = tg + tuple(node.state[1])
+      tg = tg + tuple(node.state[2])
+      if tg not in repeatCheck:
+        break
+      else:
+        repeat += 1
+        print("REPEAT STATE FOUND----------------", repeat)
+
 
     if GoalTest(node):
       print ("Success")
+      print ("Goal Node Depth: ", node.depth)
+      print ("Path: ")
+      GetPath(node)
+
       return True
 
     expandCount += 1
-#    print("Expanding State: ")
-#    PrintFormattedState(node.state)
     nodes = QueueingFunction(nodes, Expand(node, Operators()))
-#    print("Expanded to: ")
-#    for i in range(len(nodes)): 
-#      PrintFormattedState(nodes[i].state)
-#      print("gn: ", nodes[i].gn, " ", "hn: ", nodes[i].hn)
-#      print("sumCost: ", nodes[i].gn + nodes[i].hn)
-#      print("\n")
 
+
+# Driver Below
 defaultInitState = [['8','7','1'],
                     ['6','b','2'],
                     ['5','4','3']]
 
-print ("\n\n\n****PROGRAM START****")
+print ("\n\n\n****  PROGRAM START  ****")
 
 print ("Welcome to Mathew Schaffrath 8-puzzle solver.")
 print ("Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle.")
@@ -232,11 +292,11 @@ selectPuzzle = input("")
 if selectPuzzle == "2":
   userInitState = []
   print ("Enter your puzzle, use a \"b\" to represent the blank")
-  row1 = input("Enter the first row, use space or tabes between numbers   ")
+  row1 = input("Enter the first row, use space or tabs between numbers   ")
   userInitState.append(row1.split())
-  row2 = input("Enter the first row, use space or tabes between numbers   ")
+  row2 = input("Enter the first row, use space or tabs between numbers   ")
   userInitState.append(row2.split())
-  row3 = input("Enter the first row, use space or tabes between numbers   ")
+  row3 = input("Enter the first row, use space or tabs between numbers   ")
   userInitState.append(row3.split())
   print("\n")
   problem = MakeNode(userInitState)
@@ -261,7 +321,7 @@ if selectAlg == "3":
 print("Nodes expanded: ", expandCount)
 print("Max # of nodes in queue at any one time: ", queueSize)
 
-print ("*****PROGRAM END*****\n\n\n")
+print ("*****  PROGRAM END  *****\n\n\n")
 
 
 
